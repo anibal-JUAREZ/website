f30cf30c09f22071e2f69ba63a4b7101458f7bfd
Anibal-Juarez <104095878+anibal-JUAREZ@users.noreply.github.com>
2022-12-03 10:48:29 +0100

    Update CardProjet.js

--- src/components/CardProjet.js
+++ src/components/CardProjet.js
@@ -6,7 +6,7 @@ const CardProjet=(props)=> {
         <div className={classes.card}>
             <h3>{props.title}</h3>
             
-            <img src={`https://anibal-juarez-website.herokuapp.com/images/${props.img}`} alt={props.text}/>
+            <img src={`/images/${props.img}`} alt={props.text}/>
             
             <div className={classes.layer}>
                 <a target="_blank" rel="noopener noreferrer" href={props.link}><GoMarkGithub/> Voir le code</a>
